e0a796730b4da19ab13abcd7241078fda6572b06
Desertdweller1 <donovan.madel@yahoo.com>
2020-11-14 20:59:58 -0700

    op check

--- src/me/desertdweller/bettertools/listeners/PlayerJoinListener.java
+++ src/me/desertdweller/bettertools/listeners/PlayerJoinListener.java
@@ -9,7 +9,7 @@ import net.md_5.bungee.api.ChatColor;
 public class PlayerJoinListener implements Listener{
 
 	public static void onPlayerJoinEvent(PlayerJoinEvent e) {
-		if(e.getPlayer().hasPermission("bt.create")) {
+		if(e.getPlayer().hasPermission("bt.create") || e.getPlayer().isOp()) {
 			e.getPlayer().sendMessage(ChatColor.YELLOW + "Better Tools " + Bukkit.getPluginManager().getPlugin("BetterTools").getDescription().getVersion() + " is currently running. Use /bt help to get started.");
 		}
 	}
